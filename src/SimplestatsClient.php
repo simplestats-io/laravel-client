0515b430957ee474a8d858a535202369cbbc8ef6
Zacharias Creutznacher <zacharias.creutznacher@gmail.com>
2024-02-02 08:55:31 +0100

    client can send also H:i:s then we can controll in backend which we want to use...

--- src/SimplestatsClient.php
+++ src/SimplestatsClient.php
@@ -9,7 +9,7 @@ use SimpleStatsIo\LaravelClient\Jobs\SendApiRequest;
 
 class SimplestatsClient
 {
-    const TIME_FORMAT = 'Y-m-d';
+    const TIME_FORMAT = 'Y-m-d H:i:s';
 
     public function trackLogin(TrackableUser $user): PendingDispatch
     {
